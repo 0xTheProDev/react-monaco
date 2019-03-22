73af2dc5006e8b96c378a0a2d2d25b5d72da785a
Progyan Bhattacharya <bprogyan@gmail.com>
2019-03-22 23:10:32 +0530

    Environment: Prepare and Generate Boilerplate
    Yarn is package manager for the project.
    Installed babel plugins for converting into cjs and es module.
    Flow runtime for type checking in runtime and rest being stripped
    before transformation.
    Postcss to help out with css loading and vendor prefixing.
    Rollup is used for build and publish CI/CD cycle.
    
    Signed-off-by: Progyan Bhattacharya <bprogyan@gmail.com>

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss'
+import resolve from 'rollup-plugin-node-resolve';
+
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    external(),
+    babel({
+      exclude: 'node_modules/**',
+      plugins: [
+        'transform-decorators-legacy',
+        [ 'flow-runtime', { annotate: true } ],
+        'transform-flow-strip-types',
+        'external-helpers'
+      ]
+    }),
+    resolve(),
+    postcss(),
+    commonjs()
+  ]
+}
